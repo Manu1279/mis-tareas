7ea4fc317a5de4051e545fa2c837fda0a76c3a65
Manu1279 <mfontf2015@gmail.com>
2026-05-31 19:02:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ── CONFIGURACIÓN GOOGLE ──────────────────────────────────────────────────────
 // Reemplazá estos valores con los tuyos (te los voy a indicar en la Etapa 4)
-const GOOGLE_CLIENT_ID = 'TU_CLIENT_ID_AQUI';
+const GOOGLE_CLIENT_ID = '219184382354-l36ao2j9t122s362j21lc9ldvuvieher.apps.googleusercontent.com';
 const SCOPES = [
   'https://www.googleapis.com/auth/calendar.events',
   'https://www.googleapis.com/auth/gmail.send'
